c9f96fccdbb3f6837f4aafde4d90d31e46fe506a
Vgyand <gyandzhunczevv@gmail.com>
2022-03-27 20:36:54 +0300

    70

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,28 +1,29 @@
-import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialogs-reducer'
-import Dialogs from './Dialogs'
-import { connect } from 'react-redux'
-import { withAuthRedirect } from '../../hoc/withAuthRedirect'
-
+import {
+    sendMessageCreator,
+    updateNewMessageBodyCreator,
+} from "../../redux/dialogs-reducer";
+import Dialogs from "./Dialogs";
+import { connect } from "react-redux";
+import { withAuthRedirect } from "../../hoc/withAuthRedirect";
+import { compose } from "redux";
 
 let mapStateToProps = (state) => {
     return {
-        dialogsPage: state.dialogsPage
-    }
-}
+        dialogsPage: state.dialogsPage,
+    };
+};
 let mapDispatchToProps = (dispatch) => {
     return {
         sendMessage: () => {
-            dispatch(sendMessageCreator())
+            dispatch(sendMessageCreator());
         },
         updateNewMessageBody: (body) => {
-            dispatch(updateNewMessageBodyCreator(body))
-        }
-    }
-}
+            dispatch(updateNewMessageBodyCreator(body));
+        },
+    };
+};
 
-
-let AuthRedirectComponent = withAuthRedirect(Dialogs)
-
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent)
-
-export default DialogsContainer
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    withAuthRedirect
+)(Dialogs);
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,39 +1,36 @@
-import React from 'react'
-import Profile from './Profile'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
-import { getUserProfile } from '../../redux/profile-reducer'
-import { withAuthRedirect } from '../../hoc/withAuthRedirect'
-
+import React from "react";
+import Profile from "./Profile";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import { getUserProfile } from "../../redux/profile-reducer";
+import { withAuthRedirect } from "../../hoc/withAuthRedirect";
+import { compose } from "redux";
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
-        let userId = this.props.match.params.userId
+        let userId = this.props.match.params.userId;
         if (!userId) {
             userId = 22875;
         }
-        this.props.getUserProfile(userId)
+        this.props.getUserProfile(userId);
     }
 
     render() {
-
-
-        return (
-            <Profile {...this.props} profile={this.props.profile} />
-        )
+        return <Profile {...this.props} profile={this.props.profile} />;
     }
 }
 
 
-let AuthRedirectComponent = withAuthRedirect(ProfileContainer)
-
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-})
+});
 
 
-let u = withRouter(AuthRedirectComponent)
 
-export default connect(mapStateToProps, {
-    getUserProfile
-})(u)
+export default compose(
+    connect(mapStateToProps, {
+        getUserProfile,
+    }),
+    withAuthRedirect,
+    withRouter
+)(ProfileContainer);
