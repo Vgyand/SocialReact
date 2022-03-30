0521ed4d7155fb1d8b759d94058cb6b5f2185107
Vgyand <gyandzhunczevv@gmail.com>
2022-03-30 12:37:13 +0300

    71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Preloader from '../../common/Preloader/Preloader'
 import style from './ProfileInfo.module.css'
+import ProfileStatus from './ProfileStatus'
+
 
 const ProfileInfo = (props) => {
 
@@ -15,6 +17,7 @@ const ProfileInfo = (props) => {
             </div>
             <div className={style.descriptionBlock}>
                 <img src={props.profile.photos.large} alt="" />
+                <ProfileStatus status={'jija'} />
             </div>
         </div>
     )
