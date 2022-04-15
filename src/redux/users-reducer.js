7919ab86150543085d5378c01e46a839ccd78d25
Vgyand <gyandzhunczevv@gmail.com>
2022-04-15 21:17:11 +0300

    83

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -50,7 +50,7 @@ const usersReducer = (state = initialState, action) => {
         }
         case TOGGLE_IS_FOLLOWING_PROGRESS: {
             return {
-                ...state, followingInProgress: action.isFetching ? [...state.followingInProgress, action.userId] : [state.followingInProgress.filter(id => id != action.userId)],
+                ...state, followingInProgress: action.isFetching ? [...state.followingInProgress, action.userId] : [state.followingInProgress.filter(id => id !== action.userId)],
 
             }
         }
